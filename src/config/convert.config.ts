8f7563e311da356bac3f8cf0def648aab87c6c38
Thibault Jacquet <jacquett@ext.essilor.com>
2020-04-11 15:13:24 +0200

    fix: correct xe and gef scrapper

--- src/config/convert.config.ts
+++ src/config/convert.config.ts
@@ -1,1 +1,1 @@
-export const CONVERT_EURO_DOLLAR_URL = 'https://www.xe.com/fr/currencyconverter/convert/?From=Eur&To=USD&Amount=';
+export const CONVERT_EURO_DOLLAR_URL = 'https://www.xe.com/fr/currencyconverter/convert/?From=USD&To=EUR&Amount=';
